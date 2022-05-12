58cfff350416c64d58de6736a8ee397ed249113c
Zhang.H.N <zhang.h.n@foxmail.com>
2022-05-12 21:21:13 +0800

    Release v1.0

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # ddddocr-for-java
+<div style="text-align: center">
+
+![AUR](https://img.shields.io/badge/license-Apache%202.0-blue.svg)
+![Java](https://img.shields.io/badge/Java%2014.0.2-passing-success.svg)
+![Maven](https://img.shields.io/badge/Maven%203.6.3-building-success.svg)
+
+</div>
+
+基于[sml2h3](https://github.com/sml2h3)开源的[ddddocr](https://github.com/sml2h3/ddddocr)构建的java开源项目。源项目只提供pypi版本，不能满足java开发者的需求，特此尝试开发本项目。
+
+## 基本使用
+目前只支持了ddddocr的OCR功能，使用了`common_old.onnx`模型。可以通过`mvn install`安装项目或者使用[github packages源](https://github.com/GCS-ZHN/ddddocr-for-java/packages)，并添加maven依赖。
+```xml
+<dependency>
+    <groupId>top.gcszhn</groupId>
+    <artifactId>d4ocr</artifactId>
+    <version>1.0</version>
+</dependency>
+```
+下面是简单的示例代码
+```java
+import java.awt.image.BufferedImage;
+
+import top.gcszhn.d4ocr.OCREngine;
+import top.gcszhn.d4ocr.utils.IOUtils;
+
+public class Test {
+    public static void main(String[] args) {
+         OCREngine engine = OCREngine.instance();
+         BufferedImage image = IOUtils.read("AENZ.png");
+         String predict = engine.recognize(image);
+         System.out.println(predict);
+    }
+}
+```
+
+## 声明
+模型版权归原作者[sml2h3](https://github.com/sml2h3)所有，仅供学习交流使用，任何问题欢迎发issue或加入[Telegram交流群](https://t.me/zjuers)。
---
+++ src/test/java/top/gcszhn/d4ocr/OCREngineTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2022 <a href="mailto:zhang.h.n@foxmail.com">Zhang.H.N</a>.
+ *
+ * Licensed under the Apache License, Version 2.0 (thie "License");
+ * You may not use this file except in compliance with the license.
+ * You may obtain a copy of the License at
+ *
+ *       http://wwww.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language govering permissions and
+ * limitations under the License.
+ */
+package top.gcszhn.d4ocr;
+
+import org.junit.Before;
+import org.junit.Test;
+import top.gcszhn.d4ocr.utils.IOUtils;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class OCREngineTest {
+    private OCREngine engine;
+
+    @Before
+    public void beforeTest() {
+        engine = OCREngine.instance();
+    }
+
+    @Test
+    public void recognizeTest() throws IOException {
+        String[] name = {
+                "AENZ", "ALNQ", "KVXZ", "DKQT", "极速换新", "8A62N1", "九乘六等于？", "jepv"
+        };
+        for (String value : name) {
+            BufferedImage image = IOUtils.read("testData/" + value + ".png");
+            String predict = engine.recognize(image);
+            if (predict == null) predict = "";
+            System.out.printf("Predict: %s, True: %s\n", predict, value);
+        }
+    }
+}
